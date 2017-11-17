41e60f921bf5f2148cb6675e6192aa26aff49d38
Mathieu <petitjean.ma@gmail.com>
2017-11-17 16:35:18 +0100

    Why not

--- Ex10/main.cpp
+++ Ex10/main.cpp
@@ -3,27 +3,16 @@
 #include <cmath>
 
 #define LENGTH_1D	256
+#define BLK_SIZE	8
 
-void create_coeff(float * coeff_matrix)
-{
-	for (int k = 0; k < LENGTH_1D; ++k)
-	{
-		float scale = (k == 0) ? sqrt(.5) : 1.;
-		for (int n = 0; n < LENGTH_1D; ++n)
-		{	
-			coeff_matrix[n + k*LENGTH_1D] = scale * cos(M_PI*k/LENGTH_1D * (n+.5));
-		}		
-	}
-}
-
-void store(float* arrayIn, std::string filename)
+void store(float* arrayIn, std::string filename, int size)
 {
 	std::ofstream outfile;
 	outfile.open(filename, std::ios::out | std::ios::binary);
 
 	if (outfile.is_open()) 
 	{
-		outfile.write(reinterpret_cast<const char*>(arrayIn), LENGTH_1D*LENGTH_1D*sizeof(float));
+		outfile.write(reinterpret_cast<const char*>(arrayIn), size*size*sizeof(float));
 	}
 	outfile.close();
 }
@@ -43,118 +32,112 @@ void load(std::string filename, float * buffer)
 	}    
 }
 
+void create_coeff(float * coeff_matrix)
+{
+	for (int k = 0; k < BLK_SIZE; ++k)
+	{
+		float scale = (k == 0) ? sqrt(.5) : 1.;
+		for (int n = 0; n < BLK_SIZE; ++n)
+		{	
+			coeff_matrix[n + k*BLK_SIZE] = scale * cos(M_PI*k/BLK_SIZE * (n+.5));
+		}		
+	}
+}
+
 void transform(float * image, float * transformed, float * basis)
 {
 	// Row-wise
-	float * temp = new float[LENGTH_1D*LENGTH_1D];
-	for (int row = 0; row < LENGTH_1D; ++row)
+	float * temp = new float[BLK_SIZE*BLK_SIZE];
+	for (int row = 0; row < BLK_SIZE; ++row)
 	{
-		for (int elem = 0; elem < LENGTH_1D; ++elem)
+		for (int elem = 0; elem < BLK_SIZE; ++elem)
 		{
 			float sum = 0;
-			for (int k = 0; k < LENGTH_1D; ++k)
+			for (int k = 0; k < BLK_SIZE; ++k)
 			{
-				sum += image[k + row*LENGTH_1D] * basis[k + elem*LENGTH_1D];
+				sum += image[k + row*BLK_SIZE] * basis[k + elem*BLK_SIZE];
 			}
-			temp[elem + row*LENGTH_1D] = sum * sqrt(2)/sqrt(LENGTH_1D);
+			temp[elem + row*BLK_SIZE] = sum * sqrt(2)/sqrt(BLK_SIZE);
 		}
 	}
 
 	//Column-wise
-	for (int row = 0; row < LENGTH_1D; ++row)
+	for (int row = 0; row < BLK_SIZE; ++row)
 	{
-		for (int elem = 0; elem < LENGTH_1D; ++elem)
+		for (int elem = 0; elem < BLK_SIZE; ++elem)
 		{
 			float sum = 0;
-			for (int k = 0; k < LENGTH_1D; ++k)
+			for (int k = 0; k < BLK_SIZE; ++k)
 			{
-				sum += temp[row + k*LENGTH_1D] * basis[k + elem*LENGTH_1D];
+				sum += temp[row + k*BLK_SIZE] * basis[k + elem*BLK_SIZE];
 			}
-			transformed[elem + row*LENGTH_1D] = sum * sqrt(2)/sqrt(LENGTH_1D);
+			transformed[elem + row*BLK_SIZE] = sum * sqrt(2)/sqrt(BLK_SIZE);
 		}
 	}
 	delete temp;
 }
 
-void threshold(float * bufferIn, float * bufferOut, float th)
+void transpose(float * bufferIn, float * bufferOut)
 {
-	for (int i = 0; i < LENGTH_1D; ++i)
+	for (int i = 0; i < BLK_SIZE; ++i)
 	{
-		for (int j = 0; j < LENGTH_1D; ++j)
+		for (int j = 0; j < BLK_SIZE; ++j)
 		{
-			bufferOut[i + LENGTH_1D*j] = bufferIn[i + LENGTH_1D*j];
-			if (abs(bufferOut[i + LENGTH_1D*j]) < th)
+			bufferOut[i + BLK_SIZE*j] = bufferIn[j + BLK_SIZE*i];
+		}
+	}
+}
+
+void DCT1(float * bufferIn, float * bufferOut, float * DCT_basis)
+{
+	// 8 by 8 blocks of DCT, contiguous
+	float * tempIn = new float[BLK_SIZE*BLK_SIZE];
+	float * tempOut = new float[BLK_SIZE*BLK_SIZE];
+	int col;
+
+	for (int step = 0; step < pow(LENGTH_1D/BLK_SIZE,2); ++step)
+	{	
+		// fill temp with correct block
+		for (int i = 0; i < BLK_SIZE; ++i)
+		{
+			for (int j = 0; j < BLK_SIZE; ++j)
+			{	
+				col = (int) (step/32);
+				tempIn[j + i*BLK_SIZE] = bufferIn[j + (step%32)*BLK_SIZE + LENGTH_1D*col*BLK_SIZE + i*LENGTH_1D];
+			}
+		}
+
+		transform(tempIn, tempOut, DCT_basis);
+
+		// fill result image at the correct position
+		for (int i = 0; i < BLK_SIZE; ++i)
+		{
+			for (int j = 0; j < BLK_SIZE; ++j)
 			{
-				bufferOut[i + LENGTH_1D*j] = 0.;
+				col = (int) (step/32);
+				bufferOut[j + (step%32)*BLK_SIZE + LENGTH_1D*col*BLK_SIZE + i*LENGTH_1D] = tempOut[j + i*BLK_SIZE];
 			}
 		}
 	}
-}
 
-void transpose(float * bufferIn, float * bufferOut)
-{
-	for (int i = 0; i < LENGTH_1D; ++i)
-	{
-		for (int j = 0; j < LENGTH_1D; ++j)
-		{
-			bufferOut[i + LENGTH_1D*j] = bufferIn[j + LENGTH_1D*i];
-		}
-	}
-}
-
-float computePsnr(float* image_noisy, float* image_ref, float max)
-{
-	float mse = 0;
-	for (int i = 0; i < LENGTH_1D; i++)
-	{
-		for (int j = 0; j < LENGTH_1D; j++)
-		{	
-			mse += pow((image_noisy[j+i*LENGTH_1D] - image_ref[j+i*LENGTH_1D]),2);
-		}
-	}
-	mse /= LENGTH_1D*LENGTH_1D;
-
-	return 10*log10(max*max/mse);
-}
-
-void quantize8bpp(float * bufferIn, float * bufferOut)
-{
-	// find max and min values
-	float min = bufferIn[0];
-	float max = bufferIn[0];
-	for (int i = 1; i < LENGTH_1D*LENGTH_1D; ++i)
-	{
-		if( bufferIn[i] > max)
-		{
-			max = bufferIn[i];
-		}
-		if (bufferIn[i] < min)
-		{
-			min = bufferIn[i];
-		}
-	}
-
-	float ratio = (max - min);
-	for (int i = 0; i < LENGTH_1D*LENGTH_1D; ++i)
-	{
-		bufferOut[i] = (bufferIn[i]-min)/ratio*255.0;
-	}
+	delete tempIn;
+	delete tempOut;
 }
 
 int main()
 {
-	// Compute DCT basis functions
-	float * DCT_basis = new float[LENGTH_1D*LENGTH_1D];
+	// Generate DCT 8x8 basis
+	float * DCT_basis = new float[BLK_SIZE*BLK_SIZE];
 	create_coeff(DCT_basis);
 
-	// Perform a DCT
+	// Read image
 	float * lena = new float[LENGTH_1D*LENGTH_1D];
-	float * DCT_lena = new float[LENGTH_1D*LENGTH_1D];
 	load("lena_256x256.raw", lena);
-	transform(lena, DCT_lena, DCT_basis);
-	store(DCT_lena, "DCT_lena.raw");
 
-
+	// Image with 8x8 DCT
+	float * DCT88 = new float[LENGTH_1D*LENGTH_1D];
+	DCT1(lena, DCT88, DCT_basis);
+	store(DCT88, "DCT88.raw", LENGTH_1D);
 
 	return 0;
 }
