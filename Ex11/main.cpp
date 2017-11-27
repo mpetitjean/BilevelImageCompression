501fe637341930f8c40be24313d070f04ed63863
Mathieu <petitjean.ma@gmail.com>
2017-11-27 17:30:30 +0100

    End of session

--- Ex11/main.cpp
+++ Ex11/main.cpp
@@ -19,15 +19,31 @@ void load(std::string filename, float * buffer)
 	}    
 }
 
-float * encode_rle(float * bufferIn)
+int encode_rle(float * bufferIn, int * encoded)
 {
+	int runs = 0;
+	int num = 0;
+	float previous = bufferIn[0];
+
 	for (int i = 0; i < LENGTH_1D; ++i)
 	{
 		for (int j = 0; j < LENGTH_1D; ++j)
 		{
-			
+			if (bufferIn[j + i*LENGTH_1D] != previous)
+			{
+				previous = bufferIn[j + i*LENGTH_1D];
+				encoded[num] = runs;
+				runs = 0;
+				num ++;
+			}
+			else
+			{
+				runs ++;
+			}
 		}
 	}
+
+	return num;
 }
 
 int main()
@@ -37,6 +53,11 @@ int main()
 	load("earth_binary_256x256.raw", earth);	
 
 	// Encode RLE
+	int * encoded = new int[LENGTH_1D*LENGTH_1D];
+	int size = encode_rle(earth, encoded);
+
+	std::cout << size << std::endl;
+	
 
 	return 0;
 }
